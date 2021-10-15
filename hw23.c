17d03a7c02a692f4a426a246d8f33de5cd62164e
godfilld <54270565+godfilld@users.noreply.github.com>
2021-10-15 11:32:46 +0300

    correct dz3

--- hw23.c
+++ hw23.c
@@ -5,6 +5,7 @@ int main() {
     FILE * f = fopen("graph.gv", "w");
     int row, col;
     int i, j, m = 0;
+    int sum = 1;
     printf("Ввелите количество стобцов матрицы: ");
     scanf("%d", & row);
     printf("Введите количество строк матрицы: ");
@@ -33,11 +34,37 @@ int main() {
     }
     fprintf(f, "}");
     fclose(f);
-    if (j < i-1)
-        printf("Введенный граф не связанный\n");
-    else
-        printf("Введенный граф связанный\n");
-    
+    int* flag = (int*)malloc(col * sizeof(int));
+	flag[0] = 1;
+	for (int i = 1; i < col; i++)
+		flag[i] = 0;
+	for (int i = 0; i < col; i++)
+	{
+
+		if (flag[i] == 1)
+		{
+			for (int j = 0; j < col; j++)
+			{
+				if (flag[j] != 1)
+				{
+
+					for (int m = 0; m < row; m++)
+					{
+						if ((a[i][m] == 1) && (a[j][m] == 1))
+						{
+							flag[j] = 1;
+							sum++;
+
+							break;
+						}
+					}
+				}
+			}
+		}
+	}
+	if (sum == col)
+		printf("Введенный граф связанный\n");
+	else printf("Введенный граф не связанный");
 
     system("dot -Tpng graph.gv -o '2.png'");
     system("wslview 2.png");
